e439f1e00e0e04ffc9a355b3b5b94bc81ed65f8b
kaishibooo <>
2018-04-02 10:08:46 +0900

    ローカルリポジトリへコミット

---
+++ src/main/java/jp/example/dmuser/DmuserApplication.java
@@ -0,0 +1,12 @@
+package jp.example.dmuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmuserApplication.class, args);
+	}
+}
